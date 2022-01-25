139cfd5119d876601b6792a798a783e39f8b833f
Mitjanit <42413333+mitjanit@users.noreply.github.com>
2022-01-25 18:18:46 +0100

    REACT Clock

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import Clock from './components/Clock';
+import Header from './components/Header';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            title: 'React Clock'
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <Header title={this.state.title} />
+                <Clock />
+            </div>
+        );
+    }
 }
 
 export default App;
